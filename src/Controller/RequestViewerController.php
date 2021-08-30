242c06bd87281bf20d2dc25efd149d3f5585a28e
DtDPro <mike@dtdpro.com>
2021-08-30 16:06:49 -0400

    Save bins to session

--- src/Controller/RequestViewerController.php
+++ src/Controller/RequestViewerController.php
@@ -6,28 +6,50 @@ use App\Entity\Bin;
 use http\Cookie;
 use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RequestViewerController extends AbstractController
 {
+    private $requestStack;
+    private $bins;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+
+        $session = $this->requestStack->getSession();
+
+        if (!$this->bins = $session->get('bins')) {
+            $this->bins = [];
+            $session->set('bins',$this->bins);
+        }
+
+    }
+
     #[Route('/', name: 'homepage')]
     public function home(): Response
     {
-        return $this->render('request_viewer/home.html.twig', []);
+        return $this->render('request_viewer/home.html.twig', ['bins'=>$this->bins]);
     }
 
     #[Route('/create', name: 'bin_create')]
     public function create(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
+        $session = $this->requestStack->getSession();
 
         $bin = new Bin();
         $bin->setExtId(Uuid::uuid4());
 
         $entityManager->persist($bin);
         $entityManager->flush();
+
+        $this->bins[] = $bin->getExtId();
+
+        $session->set('bins',$this->bins);
 
         return $this->redirectToRoute('bin_view', ['id' => $bin->getExtId()]);
     }
@@ -42,7 +64,8 @@ class RequestViewerController extends AbstractController
 
         return $this->render('request_viewer/view.html.twig', [
             'bin' => $bin,
-            'viewUrl' => $viewUrl
+            'viewUrl' => $viewUrl,
+            'bins'=>$this->bins
         ]);
     }
 }
